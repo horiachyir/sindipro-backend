6c7bfb767084ef889cee5c6824a820d6d1ff12ad
horiachyir <horiachyir@gmail.com>
2025-07-30 14:32:18 +0000

    .

--- auth_system/urls.py
+++ auth_system/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 from . import views
-
+#hello
 urlpatterns = [
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
