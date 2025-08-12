4586f7dc9245a922c7a8957be2d0662907bd027b
horiachyir <horiachyir@gmail.com>
2025-08-12 13:09:38 +0000

    fixed POST /api/consumption/register

--- consumptions/urls.py
+++ consumptions/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.create_consumption_register, name='create_consumption_register'),
+    path('register/', views.create_consumption_register, name='create_consumption_register'),
     path('account/', views.create_consumption_account, name='create_consumption_account'),
 ]
